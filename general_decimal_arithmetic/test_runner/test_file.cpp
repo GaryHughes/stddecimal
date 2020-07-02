081bc4c58dc88e7f19d53032e9d907818191f587
Gary Hughes <garyedwardhughes@gmail.com>
2020-07-02 15:45:45 +1000

    Parse tests.

--- general_decimal_arithmetic/test_runner/test_file.cpp
+++ general_decimal_arithmetic/test_runner/test_file.cpp
@@ -3,6 +3,7 @@
 #include <regex>
 #include <iostream>
 #include <boost/algorithm/string/trim_all.hpp>
+#include <boost/algorithm/string/split.hpp>
 #include "test_context.hpp"
 
 void test_file::process(const std::string& filename)
@@ -21,7 +22,7 @@ void test_file::process(const std::string& filename)
     // keyword: value
     const std::regex directive_regex("^([^:]+):(.+)");
     // id operation operand1 operand2 operand3 -> result conditions
-    const std::regex test_regex("^\\s*(\\S+)\\s+(\\S+)\\s+((\\S+)\\s+){1,3}->\\s+(\\S+)\\s*(.*)");
+    const std::regex test_regex("^\\s*(\\S+)\\s+(\\S+)\\s+(.+\\s+){1,3}->\\s+(\\S+)\\s*(.*)");
     
     test_context context;
 
@@ -53,13 +54,39 @@ void test_file::process(const std::string& filename)
         }
 
         if (std::regex_match(line, match, test_regex)) {
-            std::cout << "TEST " << match[1].str() << " " << match[2].str() << std::endl;
+            auto id = match[1].str();
+            auto operation = match[2].str();
+            std::vector<std::string> operands;
+            auto operand_string{match[3].str()};
+            boost::trim(operand_string);
+            boost::split(operands, operand_string, boost::is_any_of("\t "), boost::token_compress_on);
+            auto result = match[4];
+            std::vector<std::string> conditions;
+            auto condition_string{match[5].str()};
+            boost::trim(condition_string);
+            boost::split(conditions, condition_string, boost::is_any_of("\t "), boost::token_compress_on);
+            process_test(id, operation, operands, result, conditions);
             continue;
         }
 
 
-        std::cout << "UNKNOWN [" << line << "]" << std::endl;
+        throw std::runtime_error("Unrecognised line [" + line + "]");
     }
+}
+
+void test_file::process_test(const std::string& id,
+                             const std::string& operation, 
+                             const std::vector<std::string>& operands, 
+                             const std::string& result, 
+                             const std::vector<std::string>& conditions)
+{
+    // std::cout << "TEST " << id << " " << operation << " OP " << operands.size() << " -> " << result << " COND " << conditions.size() << std::endl;
+    // for (const auto& operand : operands) {
+    //     std::cout << "  " << operand << '\n';
+    // }
+
+
+
 }
 
 
--- general_decimal_arithmetic/test_runner/test_file.hpp
+++ general_decimal_arithmetic/test_runner/test_file.hpp
@@ -2,12 +2,22 @@
 #define stddecimal_general_decimal_arithmetic_test_runner_test_file_hpp
 
 #include <string>
+#include <vector>
 
 class test_file
 {
 public:
 
     void process(const std::string& filename);
+
+private:
+
+    void process_test(const std::string& id,
+                      const std::string& operation, 
+                      const std::vector<std::string>& operands, 
+                      const std::string& result, 
+                      const std::vector<std::string>& conditions);
+
 
 };
 
