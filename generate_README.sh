09d67b37994c28f840add649ab859c6e4410efc1
Gary Hughes <garyedwardhughes@gmail.com>
2024-07-21 12:27:43 +1000

    Move the test result tables into collapsible sections.

--- generate_README.sh
+++ generate_README.sh
@@ -5,45 +5,69 @@ cp README_header.md README.md
 echo "## Subset Arithmetic" >> README.md
 
 if [ -f subset_32_results.md ]; then
+    echo "<details>" >> README.md
+    echo "<summary>decimal 32</summary>" >> README.md
+    echo "" >> README.md
     echo "|Type|Operation|Passed|Failed|Skipped|" >> README.md
     echo "|:---|:--------|:----:|:----:|:-----:|" >> README.md
     cat subset_32_results.md >> README.md
     echo "" >> README.md
+    echo "</details>" >> README.md
 fi
 
 if [ -f subset_64_results.md ]; then
+    echo "<details>" >> README.md
+    echo "<summary>decimal 64</summary>" >> README.md
+    echo "" >> README.md
     echo "|Type|Operation|Passed|Failed|Skipped|" >> README.md
     echo "|:---|:--------|:----:|:----:|:-----:|" >> README.md
     cat subset_64_results.md >> README.md
     echo "" >> README.md
+    echo "</details>" >> README.md
 fi
 
 if [ -f subset_128_results.md ]; then
+    echo "<details>" >> README.md
+    echo "<summary>decimal 128</summary>" >> README.md
+    echo "" >> README.md
     echo "|Type|Operation|Passed|Failed|Skipped|" >> README.md
     echo "|:---|:--------|:----:|:----:|:-----:|" >> README.md
     cat subset_128_results.md >> README.md
     echo "" >> README.md
+    echo "</details>" >> README.md
 fi
 
 echo "## Extended Arithmetic" >> README.md
 
 if [ -f extended_32_results.md ]; then
+    echo "<details>" >> README.md
+    echo "<summary>decimal 32</summary>" >> README.md
+    echo "" >> README.md
     echo "|Type|Operation|Passed|Failed|Skipped|" >> README.md
     echo "|:---|:--------|:----:|:----:|:-----:|" >> README.md
     cat extended_32_results.md >> README.md
     echo "" >> README.md
+    echo "</details>" >> README.md
 fi
 
 if [ -f extended_64_results.md ]; then
+    echo "<details>" >> README.md
+    echo "<summary>decimal 64</summary>" >> README.md
+    echo "" >> README.md
     echo "|Type|Operation|Passed|Failed|Skipped|" >> README.md
     echo "|:---|:--------|:----:|:----:|:-----:|" >> README.md
     cat extended_64_results.md >> README.md
     echo "" >> README.md
+    echo "</details>" >> README.md
 fi
 
 if [ -f extended_128_results.md ]; then
+    echo "<details>" >> README.md
+    echo "<summary>decimal 128</summary>" >> README.md
+    echo "" >> README.md
     echo "|Type|Operation|Passed|Failed|Skipped|" >> README.md
     echo "|:---|:--------|:----:|:----:|:-----:|" >> README.md
     cat extended_128_results.md >> README.md
     echo "" >> README.md
+    echo "</details>" >> README.md
 fi
