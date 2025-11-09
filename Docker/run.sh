0ff4fb66d3c1fbba04e3080e400e58d2b45c7fd6
Gary Hughes <garyedwardhughes@gmail.com>
2025-11-10 10:25:21 +1100

    Set the working directory to the mounted volume.

--- Docker/run.sh
+++ Docker/run.sh
@@ -4,4 +4,4 @@ source "config.sh"
 
 source_path="$(realpath ..)"
 
-docker run -it --rm -v ${source_path}:/stddecimal ${build_image_name}
+docker run -it --rm -v ${source_path}:/stddecimal -w /stddecimal ${build_image_name}
